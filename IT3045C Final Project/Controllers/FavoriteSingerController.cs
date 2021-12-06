5c2d37fada4a9764088ee081b1eecb2489a16e77
CoolMaple233 <95583659+CoolMaple233@users.noreply.github.com>
2021-12-06 17:51:41 -0500

    Add files via upload

---
+++ IT3045C Final Project/Controllers/FavoriteSingerController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using IT3045C_Final_Project.Models;
+
+
+namespace IT3045C_Final_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class FavoriteSingerController : ControllerBase
+    {
+        private readonly Context _context;
+        public FavoriteSingerController(Context context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Singer>>> GetSinge()
+        {
+            return await _context.Singers.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Singer>>> GetSinger(int id)
+        {
+            var favoritesinger = await _context.Singers.FindAsync(id);
+
+            if (favoritesinger == null)
+            {
+                return await _context.Singers.Take(5).ToListAsync();
+            }
+            return new Singer[] { favoritesinger };
+        }
+        [HttpPut("{id}")]
+        private bool SingerExists(int id)
+        {
+            return _context.Singers.Any(e => e.id == id);
+        }
+        public async Task<IActionResult> PutSinger(int id, Singer singer)
+        {
+            if (id != singer.id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(singer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SingerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> PostSinger(Singer singer)
+        {
+            _context.Singers.Add(singer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetSinger), new { id = singer.id }, singer);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Singer>> DeleteSinger(int id)
+        {
+            var singer = await _context.Singers.FindAsync(id);
+            if (singer == null)
+            {
+                return NotFound();
+            }
+            _context.Singers.Remove(singer);
+            await _context.SaveChangesAsync();
+            return singer;
+        }
+    }
+
+}
