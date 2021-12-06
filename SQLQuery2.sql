5c44f0175bcb4cc9ab76d93649b4268544fb35c4
kooncewc <kooncewc@mail.uc.edu>
2021-12-06 15:50:51 -0500

    Added Table Values

---
+++ SQLQuery2.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO FavoriteFood (id,Food,Times_A_Month,Sweet_Or_Savory,Region)
+VALUES 
+('14', 'Eggs', '2', 'Sweet', 'Northeast'),
+('11', 'Beans', '2', 'Savory', 'Southwest'),
+('12', 'Biscuits and Gravy', '2', 'Sweet', 'South'),
+('13', 'Chili Burger', '2', 'Sweet', 'West');
