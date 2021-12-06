6ca3cd1aaa35ec40e567cb532091968889269024
ZLongC <95583659+ZLongC@users.noreply.github.com>
2021-12-06 17:56:03 -0500

    Singer

---
+++ IT3045C Final Project/Models/Singer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IT3045C_Final_Project.Models
+{
+    public class Singer
+    {
+        public int id { get; set; }
+        public string  SingerName{ get; set; }
+        public string  Nationality{ get; set; }
+        public string  Style{ get; set; }
+        public string  Work{ get; set; }
+
+    }
+}
